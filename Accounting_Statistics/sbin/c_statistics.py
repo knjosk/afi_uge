f676bcc77ccb86b7910c5e24304b7816dc15de06
Kenji Osaki <knjosk@mac.com>
2018-07-18 05:31:43 +0900

    add actlist

--- Accounting_Statistics/sbin/c_statistics.py
+++ Accounting_Statistics/sbin/c_statistics.py
@@ -40,15 +40,25 @@ i_mem = field_tuple.index("mem")
 i_maxvmem = field_tuple.index("maxvmem")
 i_job_class = field_tuple.index("job_class")
 
+CORES = 40
+
 queue_tuple = ('sma.q', 'smb.q', 'aps.q', 'single.q', 'intsmp.q', 'intmpi.q',
                'dmaL.q', 'dmaM.q', 'dma_01', 'dma_02', 'dma_03', 'dma_04', 'dma_05', 'dma_06', 'dma_07',
                'dma_08', 'dma_09', 'dma_10', 'dma_11', 'dma_12', 'dma_13', 'dma_14', 'dma_15', 'dma_16',
                'dma_17', 'dma_18', 'dma_19', 'dma_20', 'dma_21', 'dma_37')
 
-queue_node_tuple = ('sma.q', 'smb.q', 'aps.q',
-                    'dmaL.q', 'dmaM.q', 'dma_01', 'dma_02', 'dma_03', 'dma_04', 'dma_05', 'dma_06', 'dma_07',
-                    'dma_08', 'dma_09', 'dma_10', 'dma_11', 'dma_12', 'dma_13', 'dma_14', 'dma_15', 'dma_16',
-                    'dma_17', 'dma_18', 'dma_19', 'dma_20', 'dma_21', 'dma_37')
+queue_node_sm_tuple = ('sma.q', 'smb.q', 'aps.q')
+
+queue_node_dm_tuple = ('dmaL.q', 'dmaM.q', 'dma_01', 'dma_02', 'dma_03', 'dma_04', 'dma_05', 'dma_06', 'dma_07',
+                       'dma_08', 'dma_09', 'dma_10', 'dma_11', 'dma_12', 'dma_13', 'dma_14', 'dma_15', 'dma_16',
+                       'dma_17', 'dma_18', 'dma_19', 'dma_20', 'dma_21', 'dma_37')
+
+queue_cpu_tuple = ('single.q', 'intsmp.q', 'intmpi.q')
+queue_tss_tuple = ('intsmp.q', 'intmpi.q')
+
+nodes_dict = {'sma.default': 2, 'sma.A': 2, 'sma.B': 4, 'sma.C': 8, 'sma.D': 12, 'sma.E': 26,
+              'smb.default': 2, 'smb.A': 2, 'smb.B': 4, 'smb.C': 8, 'smb.D': 12, 'smb.E': 26,
+              'aps.default': 2, 'aps.A': 2, 'aps.B': 4, 'aps.C': 8, 'aps.D': 12, 'aps.E': 24}
 
 jc_sma_tuple = ('sma.default', 'sma.A', 'sma.B', 'sma.C', 'sma.D', 'sma.E')
 jc_smb_tuple = ('smb.default', 'smb.A', 'smb.B', 'smb.C', 'smb.D', 'smb.E')
@@ -124,6 +134,17 @@ prj_limit_dict = defaultdict(float)
 prj_exceeded_list = []
 prj_used_list = []
 
+group_limit_dict = defaultdict(float)
+group_exceeded_list = []
+group_used_list = []
+
+act_group_cputime_dict = defaultdict(float)
+act_owner_cputime_dict = defaultdict(float)
+act_prj_cputime_dict = defaultdict(float)
+act_group_tss_cputime_dict = defaultdict(float)
+act_owner_tss_cputime_dict = defaultdict(float)
+act_prj_tss_cputime_dict = defaultdict(float)
+
 
 def calc_accounting(filename, start_utime, end_utime):
     try:
@@ -145,19 +166,39 @@ def calc_accounting(filename, start_utime, end_utime):
     # print start_utime
     # print end_utime
 
+    group_limit_f = open('group_limit_pm.csv', 'r')
+
+    reader = csv.reader(group_limit_f)
+    header = next(reader)
+    for row in reader:
+        group_limit_dict[row[0]] = float(row[1])
+        # print prj_limit_dict
+        # print "row"
+        # print row
+
+    # print start_utime
+    # print end_utime
+
     for line in f:
         if line.startswith("#"):
             continue
         else:
             account_data_list = line.split(":")
+            nodes = 0
             if start_utime * 1000 <= int(account_data_list[i_start_time]) < end_utime * 1000:
+                queue_name = account_data_list[i_qname]
                 queue_cout_dict[account_data_list[i_qname]] += 1
                 queue_utime_dict[account_data_list[i_qname]] += float(account_data_list[i_ru_utime])
                 queue_stime_dict[account_data_list[i_qname]] += float(account_data_list[i_ru_stime])
                 queue_cputime_dict[account_data_list[i_qname]] += float(account_data_list[i_cpu])
                 queue_wallclock_dict[account_data_list[i_qname]] += float(account_data_list[i_ru_wallclock])
-                queue_ocutime_dict[account_data_list[i_qname]] += float(account_data_list[i_ru_wallclock]) * int(account_data_list[i_slots])
                 queue_slots_dict[account_data_list[i_qname]] += int(account_data_list[i_slots])
+                if queue_name not in queue_cpu_tuple:
+                    if queue_name in queue_node_dm_tuple:
+                        nodes = -(-int(account_data_list[i_slots]) // CORES)
+                    elif queue_name in queue_node_sm_tuple:
+                        nodes = nodes_dict[account_data_list[i_job_class]]
+                        queue_ocutime_dict[account_data_list[i_qname]] += float(account_data_list[i_ru_wallclock]) * nodes
 
                 if account_data_list[i_project] == "general":
                     group_count_dict[account_data_list[i_group]] += 1
@@ -174,13 +215,13 @@ def calc_accounting(filename, start_utime, end_utime):
                     group_queue_wallclock_dict[account_data_list[i_group] + '_' + account_data_list[i_qname]] += float(account_data_list[i_ru_wallclock])
                     group_queue_slots_dict[account_data_list[i_group] + '_' + account_data_list[i_qname]] += int(account_data_list[i_slots])
 
-                    if account_data_list[i_qname] in queue_node_tuple:
-                        nodes = -(-int(account_data_list[i_slots]) // 40)
+                    if queue_name not in queue_cpu_tuple:
                         group_ocutime_dict[account_data_list[i_group]] += float(account_data_list[i_ru_wallclock]) * nodes
                         group_queue_ocutime_dict[account_data_list[i_group] + '_' + account_data_list[i_qname]] += float(account_data_list[i_ru_wallclock]) * nodes
+                    elif queue_name not in queue_tss_tuple:
+                        act_group_cputime_dict[account_data_list[i_group]] += float(account_data_list[i_cpu])
                     else:
-                        group_ocutime_dict[account_data_list[i_group]] += float(account_data_list[i_ru_wallclock]) * int(account_data_list[i_slots])
-                        group_queue_ocutime_dict[account_data_list[i_group] + '_' + account_data_list[i_qname]] += float(account_data_list[i_ru_wallclock]) * int(account_data_list[i_slots])
+                        act_group_tss_cputime_dict[account_data_list[i_group]] += float(account_data_list[i_cpu])
 
                 owner_count_dict[account_data_list[i_owner]] += 1
                 owner_utime_dict[account_data_list[i_owner]] += float(account_data_list[i_ru_utime])
@@ -215,17 +256,17 @@ def calc_accounting(filename, start_utime, end_utime):
                 prj_queue_wallclock_dict[account_data_list[i_project] + '_' + account_data_list[i_qname]] += float(account_data_list[i_ru_wallclock])
                 prj_queue_slots_dict[account_data_list[i_project] + '_' + account_data_list[i_qname]] += int(account_data_list[i_slots])
 
-                if account_data_list[i_qname] in queue_node_tuple:
-                    nodes = -(-int(account_data_list[i_slots]) // 40)
+                if queue_name not in queue_cpu_tuple:
                     owner_ocutime_dict[account_data_list[i_group]] += float(account_data_list[i_ru_wallclock]) * nodes
                     jc_ocutime_dict[account_data_list[i_job_class]] += float(account_data_list[i_ru_wallclock]) * nodes
                     prj_ocutime_dict[account_data_list[i_project]] += float(account_data_list[i_ru_wallclock]) * nodes
                     prj_queue_ocutime_dict[account_data_list[i_project] + '_' + account_data_list[i_qname]] += float(account_data_list[i_ru_wallclock]) * nodes
+                elif queue_name not in queue_tss_tuple:
+                    act_prj_cputime_dict[account_data_list[i_project]] += float(account_data_list[i_cpu])
+                    act_owner_cputime_dict[account_data_list[i_owner]] += float(account_data_list[i_cpu])
                 else:
-                    owner_ocutime_dict[account_data_list[i_owner]] += float(account_data_list[i_ru_wallclock]) * int(account_data_list[i_slots])
-                    jc_ocutime_dict[account_data_list[i_job_class]] += float(account_data_list[i_ru_wallclock]) * int(account_data_list[i_slots])
-                    prj_ocutime_dict[account_data_list[i_project]] += float(account_data_list[i_ru_wallclock]) * int(account_data_list[i_slots])
-                    prj_queue_ocutime_dict[account_data_list[i_project] + '_' + account_data_list[i_qname]] += float(account_data_list[i_ru_wallclock]) * int(account_data_list[i_slots])
+                    act_prj_tss_cputime_dict[account_data_list[i_project]] += float(account_data_list[i_cpu])
+                    act_owner_tss_cputime_dict[account_data_list[i_owner]] += float(account_data_list[i_cpu])
 
     # make Table 2-3-1 CPU usage per group
     print "--- Table 2.3 Usage per group ---"
@@ -245,27 +286,6 @@ def calc_accounting(filename, start_utime, end_utime):
         print group_cputime_dict.get(grp, 0),
         print (group_cputime_dict.get(grp, 0) / group_total_cputime) * 100
 
-    """
-   for key, value in group_queue_cputime_dict.iteritems():
-      print "key: %s,\t value:%f" %(key, value)
-
-   print "prj_queue_wallclock_dict"
-   for key, value in prj_queue_wallclock_dict.iteritems():
-      print "key: %s,\t value:%d" %(key, value)
-
-   print "prj_wallclock_dict"
-   for key, value in prj_wallclock_dict.iteritems():
-      print "key: %s,\t value:%d" %(key, value)
-
-   print "group_wallclock_dict"
-   for key, value in group_wallclock_dict.iteritems():
-      print "key: %s,\t value:%d" %(key, value)
-
-   print "prj_queue_cputime_dict"
-   for key, value in prj_queue_cputime_dict.iteritems():
-      print "key: %s,\t value:%f" %(key, value)
-
-   """
     print "--- Table 2.4 Usage per project ---"
     prj_list = prj_count_dict.keys()
     prj_total_cputime_dict = 0
@@ -301,8 +321,12 @@ def calc_accounting(filename, start_utime, end_utime):
     # print prj_limit_dict
     for key in prj_limit_dict:
         # print key
-        prj_used_list.append([key, prj_limit_dict.get(key, 0), prj_wallclock_dict.get(key, 0)])
-        if prj_limit_dict.get(key, 0) <= prj_wallclock_dict.get(key, 0):
+        prj_used_list.append([key,
+                              prj_limit_dict.get(key, 0),
+                              (prj_ocutime_dict.get(key, 0) + act_prj_cputime_dict.get(key, 0) + act_prj_tss_cputime_dict.get(key, 0)) / 60 / 60,
+                              (prj_ocutime_dict.get(key, 0) + act_prj_cputime_dict.get(key, 0)) / 60 / 60,
+                              (act_prj_tss_cputime_dict.get(key, 0)) / 60 / 60])
+        if (prj_limit_dict.get(key, 0) * 60 * 60) <= (prj_ocutime_dict.get(key, 0) + act_prj_cputime_dict.get(key, 0) + act_prj_tss_cputime_dict.get(key, 0)):
             prj_exceeded_list.append(key)
             # print key, prj_limit_dict.get(key,0),  prj_wallclock_dict.get(key,0)
             print prj_exceeded_list
@@ -318,10 +342,34 @@ def calc_accounting(filename, start_utime, end_utime):
     for x in prj_exceeded_list:
         exceeded_f.write(str(x) + "\n")
 
+    print "--- Exceeded limit group ---"
+    for key in group_limit_dict:
+        # print key
+        group_used_list.append([key,
+                                group_limit_dict.get(key, 0),
+                                (group_ocutime_dict.get(key, 0) + act_group_cputime_dict.get(key, 0) + act_group_tss_cputime_dict.get(key, 0)) / 60 / 60,
+                                (group_ocutime_dict.get(key, 0) + act_group_cputime_dict.get(key, 0)) / 60 / 60,
+                                (act_group_tss_cputime_dict.get(key, 0)) / 60 / 60])
+        if (group_limit_dict.get(key, 0) * 60 * 60) <= (group_ocutime_dict.get(key, 0) + act_group_cputime_dict.get(key, 0) + act_group_tss_cputime_dict.get(key, 0)):
+            group_exceeded_list.append(key)
+            # print key, prj_limit_dict.get(key,0),  prj_wallclock_dict.get(key,0)
+            print group_exceeded_list
+
+    group_used_f = open('group_used_pm.csv', 'w')
+
+    writer = csv.writer(group_used_f, lineterminator='\n')
+    writer.writerows(group_used_list)
+
+    group_exceeded_f = open('group_exceeded_pm.txt', 'w')
+    for x in group_exceeded_list:
+        group_exceeded_f.write(str(x) + "\n")
+
     exceeded_f.close()
     f.close()
     limit_f.close()
     used_f.close()
+    group_limit_f.close()
+    group_used_f.close()
 
 __doc__ = """{f}
 
