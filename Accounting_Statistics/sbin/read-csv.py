c302833cfe61c516831d8e8ecdd8afbf7b3a5d22
Kenji Osaki <knjosk@mac.com>
2018-07-30 10:08:43 +0900

    add temporay read-csv.py

--- Accounting_Statistics/sbin/read-csv.py
+++ Accounting_Statistics/sbin/read-csv.py
@@ -2,25 +2,34 @@
 # -*- coding: utf-8 -*-
 import csv
 from collections import defaultdict
+from docopt import docopt
+
+__doc__ = """{f}
+
+Usage:
+    {f} <used_file>
+    {f} -h | --help
+
+Options:
+    -h --help          Show this screen and exit.
+""".format(f=__file__)
+
+args = docopt(__doc__)
+used_file = args['<used_file>']
 
 exceed_list = []
 
-prj_used_f = open('/opt/uge/Accounting_Statistics/etc/prj_used_pm.csv', 'r')
-reader = csv.reader(prj_used_f)
+used_f = open(used_file, 'r')
+
+reader = csv.reader(used_f)
 for row in reader:
-    print float(row[5])
     if float(row[5]) > 0:
         exceed_list.append(row)
 
-print exceed_list
 l_exceed_list = len(exceed_list)
 message = ""
 i = 0
 while i < l_exceed_list:
-    print  exceed_list[i]
-    message += '{0[0]} is exceeded threshold {0[5]} % of limit {0[1]}(hours).\n'.format(exceed_list[i]) 
+    message += '{0[0]} is exceeded threshold {0[5]} % of limit {0[1]}(hours).\n'.format(exceed_list[i])
     i += 1
 print message
-
-    
-
